e40d8131d4c0f88a9f591298c6d5be743e92e6b5
Paul Graves <paul.graves@eca-international.com>
2023-11-17 13:39:15 +0000

    An ECA theme to docs

--- .storybook/eca-theme.js
+++ .storybook/eca-theme.js
@@ -3,7 +3,8 @@ import { create } from '@storybook/theming/create';
 export default create({
   base: 'light',
   brandTitle: 'ECA International',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://storybook.js.org/images/placeholders/350x150.png',
+  brandUrl: 'https://www.eca-international.com/',
+  brandImage:
+    'https://upload.wikimedia.org/wikipedia/commons/f/f3/ECA_logo_.png',
   brandTarget: '_self'
 });
