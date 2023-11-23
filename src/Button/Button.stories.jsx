4cd66a3cf12c53c815eaef525ee3a63f152d53ad
Paul Graves <paul.graves@eca-international.com>
2023-11-23 12:35:05 +0000

    Fix: Fix storybook

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -1,4 +1,4 @@
-import Button from './Button';
+import { Button } from './Button';
 import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
