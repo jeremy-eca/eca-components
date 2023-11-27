6aed6150e62fe2c7570aa6e79ee06069306e2680
Paul Graves <paul.graves@eca-international.com>
2023-11-27 12:18:29 +0000

    Feat: Remove Test word from BrandTest

--- src/BrandTest/BrandTest.jsx
+++ src/BrandTest/BrandTest.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const BrandTest = () => {
   return (
     <>
-      <button className='bg-red-600'>Red 600 Test</button>
-      <button className='bg-red-200'>Red 200 Test</button>
+      <button className='bg-red-600'>Red 600</button>
+      <button className='bg-red-200'>Red 200</button>
     </>
   );
 };
