adc50ba3c629b60d22295ccbb2aa752791c00618
Paul Graves <paul.graves@eca-international.com>
2023-11-28 16:29:35 +0000

    Feat: Change BrandTest

--- src/BrandTest/BrandTest.jsx
+++ src/BrandTest/BrandTest.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function BrandTest() {
   return (
     <>
-      <button className='bg-red-600'>Red 600</button>
+      <button className='bg-red-600'>Red 600 Test</button>
       <button className='bg-red-200'>Red 200</button>
     </>
   );
