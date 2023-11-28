967039f3ba3dacbc179b2fff347cee42f728cd9b
Paul Graves <147142052+paulgraveseca@users.noreply.github.com>
2023-11-28 16:47:29 +0000

    Feat: Change BrandTest (#26)
    * Feat: Change BrandTest
    
    * Feat: Change name of build pipelines
    
    * Feat: Change name of build pipelines

--- src/BrandTest/BrandTest.jsx
+++ src/BrandTest/BrandTest.jsx
@@ -4,7 +4,7 @@ export default function BrandTest() {
   return (
     <>
       <button className='bg-red-600'>Red 600 Test</button>
-      <button className='bg-red-200'>Red 200</button>
+      <button className='bg-red-200'>Red 200 Test</button>
     </>
   );
 }
