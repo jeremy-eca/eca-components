3b577b2f38a38b8ea337097bffef59d81e63a6ff
Glenn Knight <153575049+GlennKnightECA@users.noreply.github.com>
2024-02-09 10:46:34 +0000

    Feat: Add tab component (#101)

---
+++ src/Tab/Tab.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Tab(props) {
+  const { id, name, size = 'medium', label, selected = false, ...others } = props;
+
+  const sizes = {
+    medium: 'text-sm font-regular py-2 px-3',
+    large: 'text-base font-medium py-2.5 px-3.5'
+  };
+
+  return (
+    <button
+      id={id || name}
+      name={name}
+      className={`group transition
+      flex items-center justify-center
+      text-neutral-body
+      outline-none
+      disabled:default-transparent disabled:cursor-not-allowed
+      p-1 rounded-lg
+      ${selected ? 'bg-controls-highlight-paler disabled:bg-controls-highlight-palest disabled:opacity-60' : 'bg-default-transparent hover:bg-controls-highlight-pale active:bg-controls-highlight-palest disabled:bg-default-transparent disabled:text-controls-content-disabled disabled:opacity-90'}
+      `}
+      type='button'
+      role='tab'
+      aria-selected={selected}
+      {...others}
+    >
+      <div
+        className={`
+      py-2 px-3 rounded-md leading-5
+      outline outline-2 outline-default-transparent
+      group-focus-visible:outline-offset-[-2px]
+      ${selected ? 'group-focus-visible:outline-neutral-layer-2' : 'group-focus-visible:outline-neutral-detail-boldest'}
+      ${sizes[size]}
+      `}
+      >
+        {label}
+      </div>
+    </button>
+  );
+}
+
+Tab.propTypes = {
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string,
+  type: PropTypes.string,
+  size: PropTypes.string,
+  label: PropTypes.string.isRequired,
+  selected: PropTypes.bool
+};
---
+++ src/Tab/Tab.stories.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Tab } from './Tab';
+
+export default {
+  component: Tab,
+  title: 'Components/Tab',
+  parameters: {
+    componentSubtitle: 'A standard tab component'
+  },
+  argTypes: {
+    size: {
+      control: 'select',
+      description: 'Defines the size of the tab, medium is the default size',
+      options: ['medium', 'large']
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disables the tab'
+    },
+    selected: {
+      table: { disable: true }
+    },
+    type: {
+      table: { disable: true }
+    },
+    className: {
+      table: { disable: true }
+    },
+    name: {
+      table: { disable: true }
+    },
+    children: {
+      table: { disable: true }
+    },
+    onClick: {
+      table: { disable: true }
+    },
+    label: {
+      table: { disable: true }
+    },
+    id: {
+      table: { disable: true }
+    }
+  },
+  args: {
+    disabled: false,
+    size: 'medium',
+    label: 'Lorem Ipsum'
+  }
+};
+
+export const Default = {
+  args: {
+    name: 'default'
+  },
+  render: (args) => (
+    <div className='flex gap-4 justify-start'>
+      <Tab {...args} id='tabOne' label='Tab One' selected />
+      <Tab {...args} id='tabTwo' label='Tab Two' />
+    </div>
+  )
+};
+
+export const Unselected = {
+  args: {
+    name: 'unselected'
+  }
+};
+
+export const Selected = {
+  args: {
+    name: 'selected',
+    selected: true
+  }
+};
+
+export const Medium = {
+  args: {
+    name: 'medium'
+  }
+};
+
+export const Large = {
+  args: {
+    name: 'large',
+    size: 'large'
+  }
+};
