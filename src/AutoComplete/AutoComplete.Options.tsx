e69ce111ff148b794fd621a9bb3a6548c4d583c6
Rex <rexebin@users.noreply.github.com>
2024-07-24 09:39:33 +0100

    fix: append to existing class names for auto complete options (#261)
    feat: add variant and size to select component

--- src/AutoComplete/AutoComplete.Options.tsx
+++ src/AutoComplete/AutoComplete.Options.tsx
@@ -3,13 +3,14 @@ import { Combobox, Transition } from '@headlessui/react';
 
 export function AutoCompleteOptions({
   onClose,
+  className,
   ...props
 }: ComponentPropsWithRef<typeof Combobox.Options> & {
   onClose?: () => void;
 }) {
   return (
     <Transition as={Fragment} leave='transition ease-in duration-150' leaveFrom='opacity-100' leaveTo='opacity-0' afterLeave={() => onClose && onClose()}>
-      <Combobox.Options className='absolute mt-2 max-h-60 w-full overflow-auto rounded border border-neutral-detail-paler bg-neutral-layer-2 shadow-lg paragraph-sm-lighter focus:outline-none' {...props} />
+      <Combobox.Options className={`absolute mt-2 max-h-60 w-full overflow-auto rounded border border-neutral-detail-paler bg-neutral-layer-2 shadow-lg paragraph-sm-lighter focus:outline-none ${className}`} {...props} />
     </Transition>
   );
 }
