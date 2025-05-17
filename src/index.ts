a88d9fb2e884239aafe31520afed7144f0c511e9
jeremy-eca <jeremy.snyman@eca-international.com>
2025-05-17 08:09:53 +0100

    Convert to Regular App

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,10 @@
-import { Button } from './Button/Button.tsx';
-import { Checkbox } from './Checkbox/Checkbox.tsx';
-import { IconButton } from './IconButton/IconButton.tsx';
-import { Radio } from './Radio/Radio.tsx';
-import { Switch } from './Switch/Switch.tsx';
-import { Tab } from './Tab/Tab.tsx';
-import { TabGroup } from './TabGroup/TabGroup.tsx';
-import { TextArea } from './TextArea/TextArea.tsx';
-import { TextInput } from './TextInput/TextInput.tsx';
-import { Chip } from './Chip/Chip.tsx';
-import { Tooltip } from './Tooltip/Tooltip.tsx';
-import { SegmentedControl } from './SegmentedControl/SegmentedControl.tsx';
-import { Stepper, Step } from './Stepper/index.ts';
-import { Avatar } from './Avatar/index.ts';
-import { Progress } from './Progress/Progress.tsx';
-import { Menu, MenuButton, MenuItem, MenuItems } from './Menu/index.ts';
-import { NavigationMenu } from './NavigationMenu/index.ts';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+import './index.css';
 
-import { Card, CardBody, CardFooter, CardHeader } from './Card/index.ts';
-import { tailwindPlugin } from './tailwindPlugin.ts';
-import { AutoComplete, AutoCompleteInput, AutoCompleteOption, AutoCompleteOptions, HighlightMatched, DefaultOptionViewer } from './AutoComplete/index.ts';
-import { Label, FieldSet } from './Form/index.ts';
-import { Select } from './Select/index.ts';
-import { Alert } from './Alert/Alert.tsx';
-import { ActionableAlert } from './ActionableAlert/ActionableAlert.tsx';
-import { AccountMenu } from './AccountMenu/AccountMenu.tsx';
-import { Slider } from './Slider/Slider.tsx';
-
-export { Card, CardBody, CardFooter, CardHeader };
-export { Button };
-export { Progress };
-export { Checkbox };
-export { IconButton };
-export { Radio };
-export { Switch };
-export { Tab };
-export { TabGroup };
-export { TextArea };
-export { TextInput };
-export { Chip };
-export { Tooltip };
-export { SegmentedControl };
-export { Stepper, Step };
-export { AutoComplete, AutoCompleteOptions, AutoCompleteOption, AutoCompleteInput, DefaultOptionViewer, HighlightMatched };
-export { Label, FieldSet };
-export { Select };
-export { Avatar };
-export { Menu, MenuButton, MenuItem, MenuItems };
-export { Alert, ActionableAlert };
-export { NavigationMenu };
-export { AccountMenu };
-export { Slider };
-
-// Export the tailwind plugin
-export default tailwindPlugin;
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 import { createThemes } from 'tw-colors';
 import ecaDark from '@ecainternational/eca-design-tokens/tailwind/eca-dark-theme.tailwind';
 import ecaLight from '@ecainternational/eca-design-tokens/tailwind/eca-light-theme.tailwind';
-import { tailwindPlugin } from './src/tailwindPlugin.ts';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   plugins: [
-    tailwindPlugin,
     createThemes({
       'eca-light': ecaLight,
       'eca-dark': ecaDark
